50bb681fbc1c81afabec3c1883855724d042c2d0
Marc Fournier <marc.fournier91@gmail.com>
2016-12-26 14:21:43 -0500

    All routes in back end are working properly.

--- node-backend-starter-code/server.js
+++ node-backend-starter-code/server.js
@@ -10,23 +10,23 @@ app.use(bodyParser.json());
 
 app.use('/', express.static(path.join(__dirname, 'public')));
 
-app.get('/favorites', function(req, res){
-  var data = fs.readFileSync('./data.json');
+app.get('/favorites', function(req, res){ //get favorite movies
+  var data = fs.readFileSync('./data.json'); // read data from the file
   res.setHeader('Content-Type', 'application/json');
-  res.send(data);
+  res.send(data); //respond with the data
 });
 
-app.get('favorites', function(req, res){
+app.post('/favorites', function(req, res){ //post a movie to favorites
   if(!req.body.name || !req.body.oid){
-    res.send("Error");
+    res.send("Error"); //respond with error if request is missing name or oid
     return;
   }
 
-  var data = JSON.parse(fs.readFileSync('./data.json'));
-  data.push(req.body);
-  fs.writeFile('./data.json', JSON.stringify(data));
+  var data = JSON.parse(fs.readFileSync('./data.json')); //get all the data from file and set to a variable
+  data.push(req.body); //add body of request to variable
+  fs.writeFile('./data.json', JSON.stringify(data)); // save the variables new data to the file
   res.setHeader('Content-Type', 'application/json');
-  res.send(data);
+  res.send(data); //send back the new data as a response
 });
 
 app.listen(3000, function(){
